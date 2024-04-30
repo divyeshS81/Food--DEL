eedd1c2dafc5ca3ca1a4f5c317acbf69f1bc3f5c
divyeshS81 <divyeshsarvaiya999@gmail.com>
2024-04-30 17:58:21 +0530

    create a header file

--- forntend/src/App.jsx
+++ forntend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/Cart' element={<Cart/>} /> 
+        <Route path='/order' element={<PlaceOrder/>} />
       </Routes>
     </div>
   )
