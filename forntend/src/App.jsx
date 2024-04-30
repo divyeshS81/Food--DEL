239553fc7ee65ee2598f34b756241fe0649d05cf
divyeshS81 <divyeshsarvaiya999@gmail.com>
2024-04-30 17:57:09 +0530

    in this commit i create a navigation bar & pages & setup React Router

--- forntend/src/App.jsx
+++ forntend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/Cart' element={<Cart/>} /> 
+      </Routes>
+    </div>
   )
 }
 
